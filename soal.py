f3f201d1bbe25917e478e198dd75f0f878e6c3fe
daiccy21 <dannielsantosa@gmail.com>
2025-02-13 16:23:04 +0700

    Add files via upload

---
+++ soal.py
@@ -0,0 +1,5 @@
+tinggi_badan = float(input("masukkan tinggi badan lu(m): "))
+bmi = float(input("masukan body Mass Index(BMI)yang diharapkan: "))
+berat_badan = bmi*tinggi_badan**2
+
+print ("berat badan yang diperlukan adalah", berat_badan, "kg")
